a8c3d8d6a54d759b0e0da7d7fba08398150c9647
nwabisa <dlonwa001@gmail.com>
2021-07-01 10:14:53 +0200

    added things on schedule file

--- src/views/Schedule/Schedule.js
+++ src/views/Schedule/Schedule.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
-import { Button, Card } from "@material-ui/core";
 import styled from "styled-components";
+import { Button, Card, Dialog, TextField } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
-//import { Table } from "reactstrap";
-import faker, { date } from "faker";
+import faker from "faker";
 import fakerE from "faker-extra";
-import { format} from "date-fns";
-import 
+import { format } from "date-fns";
+//import { Grid } from "@material-ui/core";
+
+const SHOOT_TYPES = ["Wedding", "Babyshower", "Profile", "Birthday"];
+
+const FAKE_PROPS = {
+  shoots: fakerE.array(20, () => ({
+    id: faker.datatype.uuid(),
+    customer: faker.name.findName(),
+    date: faker.date.future(),
+    type: faker.random.arrayElement(SHOOT_TYPES),
+  })),
+};
 
 const Page = styled.div`
   display: flex;
@@ -21,6 +31,7 @@ const Page = styled.div`
 
 const StyledCard = styled(Card)`
   padding: 1rem;
+  margin: 1rem;
 `;
 
 const StyledButton = styled(Button)`
@@ -28,100 +39,165 @@ const StyledButton = styled(Button)`
     margin: 0 0.5rem;
   }
 `;
-{
 
-const HeadCell= styled.th`
-border: 1px solid;
-padding: 1rem;
+const List = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
-const DataCell = styled.td`
-border: 1px solid;
-padding: 1rem;
+const ButtonArea = styled.div`
+  padding-top: 16px;
 `;
 
-  /* <style>
-div {
-  background-color: lightgrey;
-  width: 300px;
-  border: 15px solid green;
-  padding: 50px;
-  margin: 20px;
-}
-</style> */
-}
-const SHOOT_TYPES = ["Wedding", "Babyshower", "Profile", "Birthday"];
-
-// 
-const FAKE_PROPS = {
-  shoots: fakerE.array(100, () => ({
-    id: faker.datatype.uuid(),
-    customer: faker.name.findName(),
-    //     phoneNumber: faker.number(),
-    // emailAddress: faker.address(),
-    // loation: faker.
-    date: faker.date.future(),
-    type: faker.random.arrayElement(SHOOT_TYPES),
-  })),
-};
+const DialogContent = styled.div`
+  padding: 32px;
+`;
 
 const Schedule = () => {
-  const [sorting, setSorting] = useState("date-closest");
-const [search,setSe]
+  const [shoots, setShoots] = useState(FAKE_PROPS.shoots);
+  const [deleting, setDeleting] = useState(null);
+  const [editing, setEditing] = useState(null);
 
-  return (
-    <div className="body">
-      <Page>
-        <h2>Schedule</h2>
-        <br />
-        <StyledCard>
-          <StyledButton
-            style={{ color: "navy" }}
-            variant="contained"
-            href="/AddShoot"
-          >
-            <AddIcon href="/AddShoot" />
-          </StyledButton>
+  const handleEdit = (id) => {
+    if (!editing) {
+      return setEditing(shoots.find((singleShoot) => singleShoot.id === id));
+    }
 
-          <StyledButton style={{ color: "navy" }} variant="contained">
-            <EditIcon />
-          </StyledButton>
+    const newShoots = shoots.map((singleShoot) => {
+      if (singleShoot.id !== id) return singleShoot;
+      return {
+        ...singleShoot,
+        customer: editing.customer,
+      };
+    });
 
-          <StyledButton style={{ color: "navy" }} variant="contained">
-            <RemoveIcon />
-          </StyledButton>
-        </StyledCard>
-      </Page>
+    setShoots(newShoots);
+    setEditing(null);
+  };
 
-      <table>
-        <thead>
-          <tr>
-            <th>Customer</th>
-            <th>phone number</th>
-            <th>email address</th>
-            <th>Date</th>
-            <th>location</th>
-            <th>type</th>
-          </tr>
-          </thead>
+  const handleDelete = (id) => {
+    if (!deleting)
+      return setDeleting(shoots.find((singleShoot) => singleShoot.id === id));
 
-          <tbody>
-            {FAKE_PROPS.shoots.map(({ id, type, date, customer }) => {
-              return (
-                <tr key={id}>
-                <DataCell>{customer}</DataCell>
-             <DataCell>{format(date.d.MMM()}</DataCell>
-             <DataCell>{type}</DataCell>
-             {/* <DataCell>4</DataCell>
-             <DataCell>5</DataCell>
-             <DataCell>6</DataCell> */}
-           </tr>
-              );
-            })}
-          </tbody>
-      </table>
-    </div>
-  );
+    const newShoots = shoots.filter(
+      (singleShoot) => singleShoot.id !== deleting.id
+    );
+    setShoots(newShoots);
+  };
+
+  setShoots(newShoots);
+  setDeleting(null);
 };
+
+const deleteDialog = (
+  <Dialog open={!!deleting}>
+    <DialogContent>
+      <h1>Please confirm</h1>
+      <p>The following item will be deleted.</p>
+
+      <StyledCard>
+        <div>
+          <h1>{deleting && deleting.customer}</h1>
+          <h2>{deleting && format(deleting.date, "d MMMM yyyy")}</h2>
+          <h3>{deleting && deleting.type}</h3>
+        </div>
+      </StyledCard>
+
+      <ButtonArea>
+        <StyledButton
+          startIcon={<EditIcon />}
+          onClick={() => setDeleting(null)}
+          variant="contained"
+        >
+          Cancel
+        </StyledButton>
+
+        <StyledButton
+          startIcon={<RemoveIcon />}
+          onClick={() => handleDelete(deleting)}
+          variant="contained"
+        >
+          Delete
+        </StyledButton>
+      </ButtonArea>
+    </DialogContent>
+  </Dialog>
+);
+
+const editingDialog = (
+  <Dialog open={!!editing}>
+    <DialogContent>
+      <h1>Please confirm</h1>
+
+      <StyledCard>
+        <div>
+          <TextField
+            label="customer"
+            value={!!editing.customer}
+            onChange={(event) => {
+              setEditing({ ...editing, customer: event.target.value });
+            }}
+          />
+        </div>
+      </StyledCard>
+
+      <ButtonArea>
+        <StyledButton
+          startIcon={<EditIcon />}
+          onClick={() => setEditing(null)}
+          variant="contained"
+        >
+          Cancel
+        </StyledButton>
+
+        <StyledButton
+          startIcon={<RemoveIcon />}
+          onClick={() => handleEdit(editing)}
+          variant="contained"
+        >
+          Save
+        </StyledButton>
+      </ButtonArea>
+    </DialogContent>
+  </Dialog>
+);
+
+<Page>
+  <h1>Schedule</h1>
+  {deleteDialog}
+  {editingDialog}
+  <List>
+    {shoots.map(({ id, customer, date, type }) => {
+      return (
+        <StyledCard>
+          <div>
+            <h1>{customer}</h1>
+            <h2>{format(date, "d MMMM yyyy")}</h2>
+            <h3>{type}</h3>
+
+            <ButtonArea>
+              <StyledButton
+                startIcon={<EditIcon />}
+                onClick={() => handleEdit(id)}
+                variant="contained"
+              >
+                EDIT
+              </StyledButton>
+
+              <StyledButton
+                startIcon={<RemoveIcon />}
+                onClick={() => handleDelete(id)}
+                variant="contained"
+              >
+                DELETE
+              </StyledButton>
+            </ButtonArea>
+          </div>
+        </StyledCard>
+      );
+    })}
+  </List>
+</Page>;
 
 export default Schedule;
